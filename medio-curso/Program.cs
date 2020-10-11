27553a853168018f87e0f6a652210808dd7efadb
Edson Raul Cepeda Marquez <44615729+OrbitalCardinal@users.noreply.github.com>
2020-10-11 04:02:40 -0500

    Comentarios añadidos

--- medio-curso/Program.cs
+++ medio-curso/Program.cs
@@ -10,16 +10,6 @@ namespace medio_curso
     class Program
     {
         static double[][] matrix3;
-        static void imprimir(double[][] matr){
-            Console.WriteLine("MATRIZ ------");
-            for(int i = 0; i < matr.Length; i++){
-                for(int j= 0; j < matr[0].Length; j++){
-                    Console.Write($"{matr[i][j]}, ");
-                }
-                Console.Write("\n");
-            }
-            Console.WriteLine("FIN  MATRIZ ------");
-        }
 
         static void Main(string[] args)
         {
@@ -105,39 +95,40 @@ namespace medio_curso
                 throw new Exception("El numero de hilos supera la cantidad de hilos disponibles");
             }
 
-            for(int q=0; q < n; q ++) {
+            for(int q=0; q < n; q ++) { // Ciclo de iteraciones
 
                 Console.WriteLine($"Iteracion: {q}");
 
-                 // Ciclo de iteraciones
+                 
                 Thread[] hilos = new Thread[var_hilos[q]]; // Serializando hilos
-                for(int i=0; i<var_hilos[q];i++) {
+                for(int i=0; i<var_hilos[q];i++) { 
                     hilos[i] = new Thread(multiply); 
                 }
 
                 int n_block = (int) Math.Ceiling((double) newRows / var_hilos[q]);
                 int counterSeg = 0;
                 int counterBlock = 0;
-                for(int k=0; k < var_hilos[q]; k++) {//Ciclo de los hilos
-                    if(k == var_hilos[q] - 1) {
+                for(int k=0; k < var_hilos[q]; k++) { //Ciclo de los hilos
+                    if(k == var_hilos[q] - 1) { // Asignar faltante al ultimo hilo
                         n_block = newRows - counterSeg;
                     }
                     // Console.WriteLine($"Filas por bloque : {n_block} counter {counterBlock}");
-                    double[][] tempBlock = new double[n_block][];
-                    for(int i=0; i < n_block; i++) {
+                    double[][] tempBlock = new double[n_block][]; // Segmentos
+                    for(int i=0; i < n_block; i++) { //Asignando bloques (filas) a los segmentos
                         tempBlock[i] = matrix[counterBlock+i];
 
                     }
                     // imprimir(tempBlock);
-                    for(int i=0; i < n_block; i++) { // Numero de filas = bloques
+                    // Numero de filas = bloques, inicializando hilos y realizando operaciones por bloques
+                    for(int i=0; i < n_block; i++) { 
                         
-                        double[] temp = new double[tempBlock[0].Length];
+                        double[] temp = new double[tempBlock[0].Length]; // Bloque
 
-                        for(int j = 0; j < tempBlock[0].Length; j++) {
+                        for(int j = 0; j < tempBlock[0].Length; j++) { // Asignando valores de bloques
                             temp[j] = tempBlock[i][j];
                         }
-                        hilos[k] = new Thread(multiply); 
-                        hilos[k].Start(new object[] {temp,matrix2OneDim,i+counterBlock});
+                        hilos[k] = new Thread(multiply);  // Instanciando hilo
+                        hilos[k].Start(new object[] {temp,matrix2OneDim,i+counterBlock}); // Iniciando hilo
                         hilos[k].Join();
                     }
                     counterBlock += n_block;
@@ -152,23 +143,35 @@ namespace medio_curso
             }
           
         }
+
+        static void imprimir(double[][] matr) { // Imprimir matrices
+            Console.WriteLine("MATRIZ ------");
+            for(int i = 0; i < matr.Length; i++){
+                for(int j= 0; j < matr[0].Length; j++){
+                    Console.Write($"{matr[i][j]}, ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("FIN  MATRIZ ------");
+        }
          //static void multiply(double[] array1, double[] array2, int row) {
         static void multiply(object data) {
+            // Desempacar parametros
             object[] data_objects = (object[]) data;
             double[] array1 = (double[]) data_objects[0];
             double[] array2 = (double[]) data_objects[1];
             int row = (int) data_objects[2];
 
-            int interval = array1.Length;
+            int interval = array1.Length; // Intervalo de reccorido
             int counter = 0;
             double sum = 0;
             for(int i=0; i <(array2.Length / interval); i ++) { // Recorrer segundo arreglo en intervalos
                 for(int j = 0; j < interval; j++) {
                     // Console.Write(array2[counter] + ", ");
-                    sum += array1[j] * array2[counter];
+                    sum += array1[j] * array2[counter]; // Acumulacion
                     counter++;
                 }
-                matrix3[row][i]= sum;
+                matrix3[row][i]= sum; // Asignacion de valor calculado a matriz global
                 sum = 0;
                 // Console.Write("\n");
             }
