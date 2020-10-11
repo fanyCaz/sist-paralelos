25f5a1043e6d29cf76ac0e009c20a3defaf6ac04
Edson Raul Cepeda Marquez <44615729+OrbitalCardinal@users.noreply.github.com>
2020-10-11 00:36:05 -0500

    Parallel aproach matrix multiplication branch

--- medio-curso/Program.cs
+++ medio-curso/Program.cs
@@ -6,7 +6,7 @@ namespace medio_curso
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Holis");
         }
     }
 }
